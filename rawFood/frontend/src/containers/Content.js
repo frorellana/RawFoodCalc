6cee8ca3f71a679093cc25f9e1baecd56eeac842
Francheska Orellana <yudhitza1@gmail.com>
2023-09-05 18:18:52 -0400

    edited description

--- rawFood/frontend/src/containers/Content.js
+++ rawFood/frontend/src/containers/Content.js
@@ -6,7 +6,7 @@ export default function Content({handleNewSelections, handleIncItems}) {
   return(
     <div id='content'>
       <h2>Avaiable Ingridents</h2>
-      <p>Choose ingridents to make up the meat / organ / bone ratio. Upon selecting ingrident, enter weight in lbs </p>
+      <p>Choose ingridents to make up the meat / organ / bone ratio. Upon selecting ingrident, enter amount in pounds. </p>
       <section> 
         <Ingridents handleNewSelections={handleNewSelections} handleIncItems={handleIncItems}/>
       </section>
